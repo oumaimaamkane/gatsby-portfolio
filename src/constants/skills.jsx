b6f8efb554fd2a2618aee0d945140294572433fb
Oumaima Amkane <62530553+oumaimaamkane@users.noreply.github.com>
2022-03-09 18:48:16 +0100

    fixing funfact & skills

--- src/constants/skills.jsx
+++ src/constants/skills.jsx
@@ -4,14 +4,14 @@ import ProgressBar from '../constants/progress-bar.jsx';
 
 const Skills = ()=>{
     const coding = [
-        { title: 'HTML5/CSS3',bgcolor: "#d6225a", completed: 100 },
-        { title: 'PHP',bgcolor: "#d6225a", completed: 90 },
-        { title: 'LARAVEL',bgcolor: "#d6225a", completed: 80 },
-        { title: 'JAVASCRIPT',bgcolor: "#d6225a", completed: 60 },
-        { title: 'REACT JS',bgcolor: "#d6225a", completed: 50 },
+        {id:'1', title: 'HTML5/CSS3',bgcolor: "#d6225a", completed: 100 },
+        {id:'2', title: 'PHP',bgcolor: "#d6225a", completed: 90 },
+        {id:'3', title: 'LARAVEL',bgcolor: "#d6225a", completed: 80 },
+        {id:'4', title: 'JAVASCRIPT',bgcolor: "#d6225a", completed: 60 },
+        {id:'5', title: 'REACT JS',bgcolor: "#d6225a", completed: 50 },
       ];
       const design = [
-        { title: 'UI/UX',bgcolor: "#d6225a", completed: 40 },
+        {id:'1', title: 'UI/UX',bgcolor: "#d6225a", completed: 40 },
       ];
       
     return (
@@ -21,19 +21,19 @@ const Skills = ()=>{
                 <div className="row">
                     <div className="col-lg-6" style={{paddingLeft:'2em'}}>
                         <h4>Coding Skills</h4>
-                            {coding.map((item, idx) => (
+                            {coding.map((item) => (
                                 <>
                                 <span style={{marginBottom:20}}>{item.title}</span>
-                                <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+                                <ProgressBar key={item.id} bgcolor={item.bgcolor} completed={item.completed} />
                                 </>
                             ))}
                     </div>
                     <div className="col-lg-6">
                         <h4>Design Skills</h4>
-                        {design.map((item, idx) => (
+                        {design.map((item) => (
                             <>
                                 <span>{item.title}</span>
-                                <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+                                <ProgressBar key={item.id} bgcolor={item.bgcolor} completed={item.completed} />
                             </>
                         ))}
                     </div>
